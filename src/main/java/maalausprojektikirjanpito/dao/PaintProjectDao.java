78fd1e785559a22fc64d49ae6ae07d830e3689f3
Sami <thekirvesmies@hotmail.com>
2020-04-09 17:05:00 +0300

    PaintProject ja PaintProjectDao:n runkoa

--- src/main/java/maalausprojektikirjanpito/dao/PaintProjectDao.java
+++ src/main/java/maalausprojektikirjanpito/dao/PaintProjectDao.java
@@ -2,26 +2,34 @@ package maalausprojektikirjanpito.dao;
 
 import java.sql.*;
 import java.util.*;
+import java.util.stream.Collectors;
 import maalausprojektikirjanpito.domain.PaintProject;
-import maalausprojektikirjanpito.domain.User;
 
 
 public class PaintProjectDao implements Dao<PaintProject, Integer> {
     String databaseURL;
+    Integer user_id;
+    ArrayList<PaintProject> projectsCache;
     
     /**
-     * Constructs a new UserDao-object.Checks the existence of a User table in the designated database and creates one if not present. SQLITE3 used creates the database if one does not exist.
+     * Constructs a new PaintProjectDao-object.Checks the existence of a PaintProjects table in the designated database and creates one if not present.SQLITE3 used creates the database if one does not exist.
+     * @param user_id User_id of the user currently logged in
      * @param databaseURL URL of the selected database as a String
      */
-    public PaintProjectDao(String databaseURL) {
+    public PaintProjectDao(Integer user_id, String databaseURL) {
+        this.user_id = user_id;
         this.databaseURL = databaseURL;
         
         try (Connection connection = DriverManager.getConnection("jdbc:sqlite:" + databaseURL)) {
             PreparedStatement stmt = connection.prepareStatement("CREATE TABLE IF NOT EXISTS PaintProjects ("
-                + "id INTEGER, "
+                + "project_id INTEGER, "
                 + "user_id INTEGER, "
-                + "projectname VARCHAR(40), "
-                + "PRIMARY KEY (id), "
+                + "project_name VARCHAR(40), "
+                + "project_category VARCHAR(40), "
+                + "project_completed BOOLEAN, "
+                + "project_archived BOOLEAN, "
+                + "project_intrash BOOLEAN, "
+                + "PRIMARY KEY (project_id), "
                 + "FOREIGN KEY (user_id) REFERENCES Users(id)"
                 + ");");
             stmt.executeUpdate();
@@ -32,17 +40,86 @@ public class PaintProjectDao implements Dao<PaintProject, Integer> {
             System.out.println("Error: " + e);
         }
         
-        Object users = this.list();
+        this.projectsCache = (ArrayList<PaintProject>) this.list();
     }
 
     @Override
-    public PaintProject create(PaintProject object) throws SQLException { 
-        return null;
+    public PaintProject create(PaintProject project) { 
+        int id = -1;
+        if (this.projectsCache.contains(project)) {
+            return this.projectsCache.get(this.projectsCache.indexOf(project));
+        } else {
+            try (Connection connection = DriverManager.getConnection("jdbc:sqlite:" + databaseURL)) {
+                PreparedStatement stmt = connection.prepareStatement("INSERT INTO PaintProjects "
+                    + "(user_id, project_name, project_category, project_completed, project_archived, project_intrash) "
+                    + "VALUES (?,?,?,?,?,?);",
+                    Statement.RETURN_GENERATED_KEYS);
+                
+                stmt.setInt(1, this.user_id);
+                stmt.setString(2, project.getProject_name());
+                stmt.setString(3, project.getProject_category());
+                stmt.setBoolean(4, false);
+                stmt.setBoolean(5, false);
+                stmt.setBoolean(6, false);
+                
+                stmt.executeUpdate();
+                
+                ResultSet generatedKeys = stmt.getGeneratedKeys();
+                
+                if (generatedKeys.next()) {
+                    id = generatedKeys.getInt(1);
+                }
+
+                generatedKeys.close();
+                stmt.close();
+                connection.close();
+            } catch (SQLException e) {
+                System.out.println("Error: " + e);
+            }
+        }
+        
+        if (id >= 0) {
+            project = this.read(id);
+            this.projectsCache.add(project);
+            return project;
+        } else {
+            return null;
+        }
     }
 
     @Override
-    public PaintProject read(Integer key) {
-        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    public PaintProject read(Integer project_id) {
+        PaintProject project = null;
+        List<Integer> project_ids = this.projectsCache.stream().map(p -> p.getProject_id()).collect(Collectors.toList());
+        if (project_ids.contains(project_id)) {
+            return this.projectsCache.get(project_ids.indexOf(project_id));
+        } else {
+            try (Connection connection = DriverManager.getConnection("jdbc:sqlite:" + databaseURL)) {
+                PreparedStatement stmt = connection.prepareStatement("SELECT * FROM PaintProjects "
+                        + "WHERE PaintProject.project_id = (?);");
+                
+                stmt.setInt(1, project_id);
+                ResultSet rs = stmt.executeQuery();
+
+                project = new PaintProject(
+                        rs.getInt("project_id"), 
+                        rs.getInt("user_id"), 
+                        rs.getString("project_name"), 
+                        rs.getString("project_category"),
+                        rs.getBoolean("project_completed"),
+                        rs.getBoolean("project_archived"),
+                        rs.getBoolean("project_intrash")
+                );
+
+                rs.close();
+                stmt.close();
+                connection.close();           
+            } catch (SQLException e) {
+                System.out.println("Error: " + e.toString());
+            }
+        }
+        
+        return project;
     }
 
     @Override
@@ -60,15 +137,22 @@ public class PaintProjectDao implements Dao<PaintProject, Integer> {
         ArrayList<PaintProject> projects = new ArrayList<>();
         
         try (Connection connection = DriverManager.getConnection("jdbc:sqlite:" + databaseURL)) {
-            PreparedStatement stmt = connection.prepareStatement("SELECT * FROM PaintProjects");
+            PreparedStatement stmt = connection.prepareStatement("SELECT * FROM PaintProjects;");
             
-            ResultSet resultSet = stmt.executeQuery();
+            ResultSet rs = stmt.executeQuery();
             
-            while (resultSet.next()) {
-                projects.add(new PaintProject(resultSet.getString("projectname")));
+            while (rs.next()) {
+                projects.add(new PaintProject(
+                        rs.getInt("project_id"), 
+                        rs.getInt("user_id"), 
+                        rs.getString("project_name"), 
+                        rs.getString("project_category"),
+                        rs.getBoolean("project_completed"),
+                        rs.getBoolean("project_archived"),
+                        rs.getBoolean("project_intrash")));
             }
             
-            resultSet.close();
+            rs.close();
             stmt.close();
             connection.close();
             
@@ -79,4 +163,31 @@ public class PaintProjectDao implements Dao<PaintProject, Integer> {
         return null;
     }
     
+    /**
+     * Get categories of the selected user's projects.
+     * @param user_id User_id of the selected user
+     * @return List of categories of the users projects
+     */
+    public ArrayList<String> categoriesOfUser(Integer user_id) {
+        ArrayList<String> categories = new ArrayList<>();
+        
+        for (PaintProject p : this.projectsCache) {
+            if (!categories.contains(p.getProject_category())) {
+                categories.add(p.getProject_category());
+            }
+        }
+        
+        return categories;
+    }
+    
+    /**
+     * Get projects in a particular category.
+     * @param category Selected category as a String
+     * @return ArrayList<PaintProject> of the projects in a category
+     */
+    public ArrayList<PaintProject> projectsInCategory(String category) {
+        ArrayList<PaintProject> projectsInCategory = (ArrayList<PaintProject>) this.projectsCache.stream().filter(c -> c.getProject_category().equals(category)).collect(Collectors.toList());
+        
+        return projectsInCategory;
+    }
 }
--- src/main/java/maalausprojektikirjanpito/domain/PaintProject.java
+++ src/main/java/maalausprojektikirjanpito/domain/PaintProject.java
@@ -3,28 +3,111 @@ package maalausprojektikirjanpito.domain;
 import java.util.ArrayList;
 
 public class PaintProject {
-    String name;
-    ArrayList<SubProject> subprojects;
-    
+    Integer project_id;
+    Integer user_id;
+    String project_name;
+    String project_category;
+    Boolean project_completed;
+    Boolean project_archived;
+    Boolean project_intrash;
+    ArrayList<SubProject> subprojects = new ArrayList<>();
+
     /**
-     * Create a new paint project object.
-     * @param name of the project
+     * Create a new PaintProject-object.
+     * @param user_id User_id of the user whose projects are being managed as an Integer
+     * @param project_name Project's name as a String
+     * @param project_category Project's category as a String
      */
-    public PaintProject(String name) {
-        this.name = name;
-        this.subprojects = new ArrayList<>();
+    public PaintProject(Integer user_id, String project_name, String project_category) {
+        this.user_id = user_id;
+        this.project_name = project_name;
+        this.project_category = project_category;
+        this.project_completed = false;
+        this.project_archived = false;
+        this.project_intrash = false;
     }
 
-    public String getName() {
-        return name;
+    /**
+     * Create a new PaintProject-object.
+     * @param project_id Project_id of the project as an Integer
+     * @param user_id User_id of the user whose projects are being managed as an Integer
+     * @param project_name Project's name as a String
+     * @param project_category Project's category as a String
+     */
+    
+
+    public PaintProject(Integer project_id, Integer user_id, String project_name, String project_category, Boolean project_completed, Boolean project_archived, Boolean project_intrash) {
+        this.project_id = project_id;
+        this.user_id = user_id;
+        this.project_name = project_name;
+        this.project_category = project_category;
+        this.project_completed = project_completed;
+        this.project_archived = project_archived;
+        this.project_intrash = project_intrash;
     }
 
-    public void setName(String name) {
-        this.name = name;
+    public Integer getProject_id() {
+        return project_id;
     }
 
-    public ArrayList getSubprojects() {
+    public void setProject_id(Integer project_id) {
+        this.project_id = project_id;
+    }
+
+    public Integer getUser_id() {
+        return user_id;
+    }
+
+    public void setUser_id(Integer user_id) {
+        this.user_id = user_id;
+    }
+
+    public String getProject_name() {
+        return project_name;
+    }
+
+    public void setProject_name(String project_name) {
+        this.project_name = project_name;
+    }
+
+    public String getProject_category() {
+        return project_category;
+    }
+
+    public void setProject_category(String project_category) {
+        this.project_category = project_category;
+    }
+
+    public Boolean getProject_completed() {
+        return project_completed;
+    }
+
+    public void setProject_completed(Boolean project_completed) {
+        this.project_completed = project_completed;
+    }
+
+    public Boolean getProject_archived() {
+        return project_archived;
+    }
+
+    public void setProject_archived(Boolean project_archived) {
+        this.project_archived = project_archived;
+    }
+
+    public Boolean getProject_intrash() {
+        return project_intrash;
+    }
+
+    public void setProject_intrash(Boolean project_intrash) {
+        this.project_intrash = project_intrash;
+    }
+
+    public ArrayList<SubProject> getSubprojects() {
         return subprojects;
-    }    
+    }
+
+    public void setSubprojects(ArrayList<SubProject> subprojects) {
+        this.subprojects = subprojects;
+    }
     
 }
